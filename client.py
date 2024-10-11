b5256aaa47bee92052a2dfab2e9df860b15093d9
aianpar <phunkpar@gmail.com>
2024-10-10 22:22:14 -0400

    add block creation and basic sha256 hashing

--- client.py
+++ client.py
@@ -1,13 +1,33 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
 
-    def new_block(self):
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self,proof,previous_hash=None):
         #should create a new block and add it to chain
-        return
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transaction,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transaction = []
+        self.chain.append(block)
+        return block
+        
     def helloworld(self):
         print("Hello World")
+        print(hashlib.sha256("Hello World".encode()).hexdigest())
+        return
     
     def new_transaction(self,sender,recipient,amount):
         """
@@ -23,14 +43,19 @@ class Blockchain(object):
         })
         return self.last_block['index']+1
     
+    
     @staticmethod
     def hash(block):
-        #hashing
-        return
+        """
+        :param block: <dict> Block
+        :return: <str> Hash256 of block
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         #returns the last block in the chain
-        return
+        return self.chain[-1]
 
 Blockchain().helloworld()
